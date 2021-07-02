0b4b08754bfc660a9bcd5d6297467e2f204ade61
TextError <savinraul1983@yahoo.com>
2021-07-02 13:09:07 +0100

    DINAMIC API route

---
+++ pages/api/feedback/[id].js
@@ -0,0 +1,15 @@
+import fs from 'fs';
+import path from 'path';
+
+const feedbackId = (req, res) => {
+  const { id } = req.query;
+
+  const filePath = path.join(process.cwd(), 'data', 'feedback.json');
+  const fileData = fs.readFileSync(filePath);
+  const data = JSON.parse(fileData);
+  const file = data.find((el) => el.id === id);
+  res.status(200).json({ file });
+
+};
+
+export default feedbackId;
---
+++ pages/feedback/index.jsx
@@ -0,0 +1,41 @@
+import fs from 'fs';
+import path from 'path';
+import { useState } from 'react';
+
+const Feedback = ({ data }) => {
+  const [state, setState] = useState();
+
+  const onLoad = ({ id }) => {
+    fetch(`/api/feedback/${id}`)
+      .then((res) => res.json())
+      .then(({ file }) => setState(file));
+  };
+
+  return (
+    <>
+      {state && <p>{state.email}</p>}
+      <ul>
+        {data.map(({ id, text }) => (
+          <li key={id}> {text}{' '}
+            <button onClick={() => onLoad(id)}>
+              Show Details
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+
+export const getStaticProps = async () => {
+  const filePath = path.join(process.cwd(), 'data', 'feedback.json');
+  const fileData = fs.readFileSync(filePath);
+  const data = JSON.parse(fileData);
+
+  return {
+    props: { data },
+  };
+}
+
+export default Feedback;

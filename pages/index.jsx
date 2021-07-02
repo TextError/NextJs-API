dab41464f2cc94af6a03ef30a8fdc0a5b15255c7
TextError <savinraul1983@yahoo.com>
2021-07-02 11:51:44 +0100

    FrontEnd basic form

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,32 @@
+import { useRef } from "react";
+
 const Home = () => {
+  const isEmail = useRef();
+  const isText = useRef();
+
+  const onSubmit = () => {
+    e.preventDefault();
+    const email = isEmail.current.value;
+    const text = isText.current.value;
+    
+  };
+
   return (
-    <div>
-      home
-    </div>
-  )
+    <>
+      <h1>Home</h1>
+        <form noValidat onSubmit={onSubmit}>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input type="email" id='email' ref={isEmail} />
+          </div>
+          <div>
+            <label htmlFor="feedback">Feedback</label>
+            <textarea rows={5} id='feedback' ref={isText} />
+          </div>
+          <button type="submit">submit</button>
+        </form>
+    </>
+  );
 }
 
-export default Home
+export default Home;
